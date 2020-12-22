95097b4ce3d48d9385c3e9ca503270c29c754945
Gilvando Lucas Vieira <gilvandolucasvieira@gmail.com>
2020-12-22 11:18:25 -0300

    Adicionando CRUD server app

---
+++ crud-server/src/main/java/gilvando/vieira/crud/CrudApplication.java
@@ -0,0 +1,13 @@
+package gilvando.vieira.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
